aa9c61496a2ae992ec3d330078e5555e147a3a10
graziellecafe <graziellecafe@users.noreply.github.com>
2021-12-12 14:10:31 -0300

    #1 - Configurações TS e Node. Criação de uma função em TS

---
+++ functions/add-numbers.js
@@ -0,0 +1,4 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+console.log('A soma dos números ${num1} e ${num2} é: ', addNumbers(5, 10));
---
+++ functions/add-numbers.ts
@@ -0,0 +1,7 @@
+function addNumbers(num1: number, num2: number){
+    return num1 + num2; 
+}
+
+
+console.log('A soma dos números é', addNumbers(5, 10));
+
